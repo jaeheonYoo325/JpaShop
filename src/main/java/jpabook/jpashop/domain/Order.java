e019bd3854cdbabe14fa883e8bc63e2a7f6ff0e0
jaeheonYoo325 <yoojh0325@gmail.com>
2021-07-25 14:46:22 +0900

    2021.07.25 : 주문 기능 테스트 완료.

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -60,7 +60,7 @@ public class Order {
 	public void addOrderItem(OrderItem orderItem) {
 		orderItems.add(orderItem);
 		orderItem.setOrder(this);
-	}
+	} 
 	
 	public void setDelivery(Delivery delivery) {
 		this.delivery = delivery;
